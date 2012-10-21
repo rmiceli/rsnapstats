97502777cf588cc9e93b28be493775011c2f0f69
Rob Miceli <rob@cornell>
2012-10-21 12:39:10 -0400

    Use dicts instead of lists to store rsync data
    This will make storing to a data file easier and more readable.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ rsnapstats
 ==========
 
 Prints out stats from rsnapshot run. Inspired by rsnapreport.pl
+Tested with rsnapshot 1.3.1 and rsync 3.0.7
 
 Usage
 ==========
--- rsnapstats.py
+++ rsnapstats.py
@@ -1,9 +1,9 @@
 import sys
 
-def parseline(line,listvar):
+def parseline(line):
   """
     Extracts the number (float or int) from the string 'line' and
-    appends it to the list 'listvar' as a float
+    appends it as a float
   """
   dum = []
   words = line.split()
@@ -33,10 +33,21 @@ def humanize_bytes(bytes, precision=1):
           break
   return '%.*f %s' % (precision, bytes / factor, suffix)
 
+def stats_dictionary(data):
+  """
+    Return a dict of all the stats that rsync outputs
+  """
+  stats_dict = {'source':data[0],'numFiles':data[1],'numFilesTx':data[2], \
+      'fileSize':data[3],'fileSizeTx':data[4],'litData':data[5], \
+      'matchedData':data[6],'listSize':data[7],'listGen':data[8], \
+      'listTx':data[9],'bytesSent':data[10], 'bytesRec':data[11], \
+      'txSpeed':data[12],'speedup':data[13]}
+
+  return stats_dict
+
 def main():
   # initialize
-  numFiles,fileSize,numFilesTx,fileSizeTx, \
-    bytesSent,bytesRec,totalSize,source = ([] for i in range(8))
+  stats = []
   line = ''
 
   # get rsync stats
@@ -48,33 +59,48 @@ def main():
     else:
       line = line + tmp
 
-    if "Number of files transferred" in line:
-      numFilesTx.append(parseline(line,numFilesTx))
-    elif "Number of files" in line:
-      numFiles.append(parseline(line,numFiles))
-    elif "Total file size" in line:
-      fileSize.append(parseline(line,fileSize))
-    elif "Total transferred file size" in line:
-      fileSizeTx.append(parseline(line,fileSizeTx))
-    elif "Total bytes sent" in line:
-      bytesSent.append(parseline(line,bytesSent))
-    elif "Total bytes received" in line:
-      bytesRec.append(parseline(line,bytesRec))
-    elif "total size is" in line:
-      totalSize.append(parseline(line,totalSize))
-    elif "/usr/bin/rsync" in line:
+    # sort through line by line. if/elif order matters here
+    if "/usr/bin/rsync" in line:
+      data = []
       #src is always second to last argument
       #then remove user name before @
-      source.append(line.split()[-2].split('@')[-1])
+      data.append(line.split()[-2].split('@')[-1])
+    elif "Number of files" in line:
+      data.append(parseline(line))
+    elif "Number of files transferred" in line:
+      data.append(parseline(line))
+    elif "Total file size" in line:
+      data.append(parseline(line))
+    elif "Total transferred file size" in line:
+      data.append(parseline(line))
+    elif "Literal data" in line:
+      data.append(parseline(line))
+    elif "Matched data" in line:
+      data.append(parseline(line))
+    elif "File list size" in line:
+      data.append(parseline(line))
+    elif "File list generation time" in line:
+      data.append(parseline(line))
+    elif "File list transfer time" in line:
+      data.append(parseline(line))
+    elif "bytes/sec" in line:
+      data.append(parseline(line))
+    elif "Total bytes sent" in line:
+      data.append(parseline(line))
+    elif "Total bytes received" in line:
+      data.append(parseline(line))
+    elif "total size is" in line:
+      data.append(parseline(line))
+      stats.append(stats_dictionary(data))
     line = ''
 
-  for idx,val in enumerate(numFilesTx):
+  for idx,val in enumerate(stats):
     # print stats
-    print source[idx], "\n", \
-      "\t files backed-up:", int(numFiles[idx][0]), "("+humanize_bytes(fileSize[idx][0],2)+")\n", \
-      "\t files updated:", int(numFilesTx[idx][0]), "("+humanize_bytes(fileSizeTx[idx][0],2)+")\n", \
-      "\t sent/received:", humanize_bytes(bytesSent[idx][0],2), "/", humanize_bytes(bytesRec[idx][0],2),"\n", \
-      "\t speedup:", totalSize[idx][1]
+    print stats[idx]['source'], "\n", \
+      "\t files backed-up:", int(stats[idx]['numFiles'][0]), "("+humanize_bytes(stats[idx]['fileSize'][0],2)+")\n", \
+      "\t files updated:", int(stats[idx]['numFilesTx'][0]), "("+humanize_bytes(stats[idx]['fileSizeTx'][0],2)+")\n", \
+      "\t sent/received:", humanize_bytes(stats[idx]['bytesSent'][0],2), "/", humanize_bytes(stats[idx]['bytesRec'][0],2),"\n", \
+      "\t speedup:", stats[idx]['speedup'][1]
 
 if __name__ == '__main__':
   main()
